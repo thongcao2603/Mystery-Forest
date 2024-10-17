c29edc3009bef33080b9171a355069cac6c89355
thongcao2603 <133794541+thongcao2603@users.noreply.github.com>
2024-10-17 09:37:30 +0700

    move, jump player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed = 12f;
+    private float gravity = -9.81f * 2;
+    [SerializeField] private float jumpHeight = 3f;
+
+    [SerializeField] private Transform groundCheck;
+    private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+
+    Vector3 velocity;
+
+    bool isGrounded;
+    bool canDoubleJump;
+
+    Rigidbody2D rigi;
+
+    private void Awake()
+    {
+        rigi = GetComponent<Rigidbody2D>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +30,48 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        IsGrounded();
+        Movement();
+        Debug.Log(canDoubleJump);
+    }
+
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        velocity.x = x * speed;
+
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+            else if (canDoubleJump)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                canDoubleJump = false;
+            }
+        }
+        velocity.y += gravity * Time.deltaTime;
+
+        rigi.velocity = velocity;
+    }
+
+    void IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(groundCheck.position, Vector2.down, groundDistance, groundMask);
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
 
     }
 }
